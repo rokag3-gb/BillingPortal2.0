3aecf47de75864269c1d13f069ba921c99ac1e00
Youngbin Han <youngbin.han@cloudmt.co.kr>
2020-12-18 17:41:29 +0900

    전체적인 URL 설정 변경
    - 관리자: /admin -> /billingadmin
    - 포털: /portal -> /

--- Mate365BillingPortal/urls.py
+++ Mate365BillingPortal/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('portal/', include('portal.urls'))
+    path('billingadmin/', admin.site.urls),
+    path('', include('portal.urls'))
 ]
