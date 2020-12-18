01e9948238b314edf5f1f6bf3c3ad06e3fc96931
Youngbin Han <youngbin.han@cloudmt.co.kr>
2020-12-18 15:10:14 +0900

    MS-SQL 드라이버 설정 작업

--- Mate365BillingPortal/settings/base.py
+++ Mate365BillingPortal/settings/base.py
@@ -22,12 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9#2chz(hxl=*j36mczp5gmyrfl24i@=y^co2*17baph_fpge#l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,18 +63,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Mate365BillingPortal.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
