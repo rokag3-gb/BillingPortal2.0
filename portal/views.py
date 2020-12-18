066d535800306f459fdf3191bbbfa13b469c55c2
Youngbin Han <youngbin.han@cloudmt.co.kr>
2020-12-18 17:42:07 +0900

    메인 페이지 로그인 있어야만 접속 가능하도록 설정

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'portal/index.html')
