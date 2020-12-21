af5b86341da6652fdf4189f145c617bbc2649666
Youngbin Han <youngbin.han@cloudmt.co.kr>
2020-12-21 12:32:16 +0900

    암호 복구 메일 전송 폼 추가

--- portal/urls.py
+++ portal/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('auth/login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
-    path('auth/logout/', auth_views.LogoutView.as_view())
+    path('auth/login/', auth_views.LoginView.as_view(template_name='portal/auth/login.html'), name='login'),
+    path('auth/logout/', auth_views.LogoutView.as_view()),
+    path('auth/recover/', auth_views.PasswordResetView.as_view(template_name='portal/auth/recover.html'), name='recover'),
+    path('auth/recover/done', auth_views.PasswordResetDoneView.as_view(template_name='portal/auth/recover_done.html'))
 ]
