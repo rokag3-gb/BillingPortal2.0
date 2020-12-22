9ceaf87b1ac7350a00fb636f2dc8ed619bd638a7
Youngbin Han <youngbin.han@cloudmt.co.kr>
2020-12-22 15:56:58 +0900

    설정 화면과 암호 변경 기능 추가

--- portal/urls.py
+++ portal/urls.py
@@ -9,10 +9,15 @@ urlpatterns = [
     path('auth/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('auth/recover/', auth_views.PasswordResetView.as_view(
         template_name='portal/auth/recover.html'), name='password_reset'),
-    path('auth/recover/done', auth_views.PasswordResetDoneView.as_view(template_name='portal/auth/recover_done.html'),
-         name='password_reset_done'),
-    path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='portal/auth/reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('auth/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='portal/auth/reset_complete.html'),
-         name='password_reset_complete')
+    path('auth/recover/done', auth_views.PasswordResetDoneView.as_view(
+        template_name='portal/auth/recover_done.html'), name='password_reset_done'),
+    path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='portal/auth/reset_confirm.html'), name='password_reset_confirm'),
+    path('auth/reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='portal/auth/reset_complete.html'), name='password_reset_complete'),
+    path('settings/', views.settings, name='settings'),
+    path('settings/changepw', auth_views.PasswordChangeView.as_view(
+        template_name='portal/settings/changepw.html'), name='password_change'),
+    path('settings/changepw/done', auth_views.PasswordChangeDoneView.as_view(
+        template_name='portal/settings/pwchanged.html'), name='password_change_done')
 ]
