a283473f1657198e4ef9cf0f8afa2ac9a6ef28e1
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-01-05 14:52:49 +0900

    사이드바 메뉴별 페이지 구성

--- portal/views.py
+++ portal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
 
+sidebar_items = ["dashboard", "profile", "messages"]
 
 # Create your views here.
 @login_required
@@ -11,3 +12,16 @@ def index(request: HttpRequest) -> HttpResponse:
 @login_required
 def settings(request: HttpRequest) -> HttpResponse:
     return render(request, 'portal/settings/index.html')
+
+@login_required
+def dashboard(request: HttpRequest) -> HttpResponse:
+    return render(request, 'portal/dashboard.html', {'sidebar': 'dashboard', 'sidebar_items': sidebar_items })
+
+@login_required
+def profile(request: HttpRequest) -> HttpResponse:
+    return render(request, 'portal/profile.html', {'sidebar': 'profile', 'sidebar_items': sidebar_items})
+
+@login_required
+def messages(request: HttpRequest) -> HttpResponse:
+    return render(request, 'portal/messages.html', {'sidebar': 'messages', 'sidebar_items': sidebar_items})
+
