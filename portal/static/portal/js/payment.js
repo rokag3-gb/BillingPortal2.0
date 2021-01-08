dd1c20d95700b1a94d78f08828dbf4e053b57413
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-01-08 13:46:51 +0900

    날짜 처리 방식 수정

--- portal/static/portal/js/payment.js
+++ portal/static/portal/js/payment.js
@@ -3,34 +3,17 @@
   {
       var frm_pay = document.frm_pay;
 
-      var today = new Date();
-      var year  = today.getFullYear();
-      var month = today.getMonth() + 1;
-      var date  = today.getDate();
-      var time  = today.getTime();
-
-      if(parseInt(month) < 10)
-      {
-          month = "0" + month;
-      }
-
-      if(parseInt(date) < 10)
-      {
-          date = "0" + date;
-      }
-
-
       /*--공통--*/        
-      frm_pay.EP_mall_id.value        = "T5102001";                              //가맹점 ID
-      frm_pay.EP_mall_nm.value        = "클라우드메이트";                    //가맹점명
-      frm_pay.EP_order_no.value       = "ORDER_" + year + month + date + time;   //가맹점 주문번호    
+    //   frm_pay.EP_mall_id.value        = "T5102001";                              //가맹점 ID
+    //   frm_pay.EP_mall_nm.value        = "클라우드메이트";                    //가맹점명
+      frm_pay.EP_order_no.value       = `ORDER_${dayjs().format('YYYYMMDDHHmmssSSS')}`;   //가맹점 주문번호    
                                                                                  //결제수단(select)
       frm_pay.EP_currency.value       = "00";                                    //통화코드 : 00-원
       frm_pay.EP_product_nm.value     = "관리형 클라우드 서비스";                            //상품명
       frm_pay.EP_product_amt.value    = "97845";                                 //상품금액
                                                                                  //가맹점 return_url(윈도우 타입 선택 시, 분기)
-      frm_pay.EP_lang_flag.value      = "KOR"                                    //언어: KOR / ENG
-      frm_pay.EP_charset.value        = "UTF-8"                                 //가맹점 Charset: EUC-KR(default) / UTF-8
+    //   frm_pay.EP_lang_flag.value      = "KOR"                                    //언어: KOR / ENG
+    //   frm_pay.EP_charset.value        = "UTF-8"                                 //가맹점 Charset: EUC-KR(default) / UTF-8
       frm_pay.EP_user_id.value        = "sugarhill";                               //가맹점 고객 ID
       frm_pay.EP_memb_user_no.value   = "15123485756";                           //가맹점 고객 일련번호
       frm_pay.EP_user_nm.value        = "슈가힐";                                //가맹점 고객명
@@ -40,7 +23,7 @@
       frm_pay.EP_user_addr.value      = "서울시 구로구";                  //가맹점 고객 주소
       frm_pay.EP_product_type.value   = "1";                                     //상품정보구분 : 0-실물, 1-서비스
       frm_pay.EP_product_expr.value   = "";                              //서비스기간 : YYYYMMDD
-      frm_pay.EP_return_url.value     = "http://localhost:8000/payment/result";      // Return 받을 URL (HTTP부터 입력)
+    //   frm_pay.EP_return_url.value     = "http://localhost:8000/payment/result";      // Return 받을 URL (HTTP부터 입력)
 
                                       
       /*--신용카드--*/                    
@@ -55,23 +38,9 @@
                                                                                  //국민 앱카드 사용(select)                                                                                  
                                                                                                                  
       /*--가상계좌--*/                   
-      var vacct_until = new Date();
-      vacct_until.setDate(vacct_until.getDate() + 7);   
-      var vmonth =  today.getMonth() + 1
-      if(parseInt(month) < 10)
-      {
-        vmonth = "0" + vmonth;
-      }
-
-      var vdate = today.getDate()
-      if(parseInt(date) < 10)
-      {
-        vdate = "0" + vdate;
-      } 
-      console.log(`${today.getFullYear()}${vmonth}${vdate}`)
       frm_pay.EP_vacct_bank.value     = "";                                      //가상계좌 사용가능한 은행 LIST 
-      frm_pay.EP_vacct_end_date.value = `${today.getFullYear()}${vmonth}${vdate}`;    //입금 만료 날짜
-      frm_pay.EP_vacct_end_time.value = "153025";                                //입금 만료 시간
+      frm_pay.EP_vacct_end_date.value = dayjs().add(7, 'day').format('YYYYMMDD');    //입금 만료 날짜
+      frm_pay.EP_vacct_end_time.value = dayjs().add(7, 'day').format('HHMMss');                                //입금 만료 시간
       
       
 
