b42bcbf15aaf9f9383fd03e6ccb33b1fa692eddc
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-01-12 14:50:20 +0900

    기본 조직 모델 정의 추가

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Organization, Membership
 
 # Register your models here.
+@admin.register(Organization, Membership)
+class OrgAdmin(admin.ModelAdmin):
+    pass
+
