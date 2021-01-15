1987c79710bd8a78af2f4572d1297e8c58b5de74
sukso96100 <sukso96100@gmail.com>
2021-01-15 21:07:04 +0900

    결제 UI 작업

--- portal/static/portal/js/payment.js
+++ portal/static/portal/js/payment.js
@@ -1,5 +1,16 @@
 function submitPayment() {
+  const paymentForm = document.getElementById("payment-form");
+  const paymentProgress = document.getElementById("payment-progress");
+  const paymentSuccess = document.getElementById("payment-success");
+  const paymentError = document.getElementById("payment-error");
+  const paymentErrorContainer = document.getElementById(
+    "payment-error-container"
+  );
   const csrftoken = document.querySelector("[name=csrfmiddlewaretoken]").value;
+
+  paymentErrorContainer.hidden = true;
+  paymentForm.hidden = true;
+  paymentProgress.hidden = false;
   fetch("", {
     method: "POST",
     credentials: "same-origin",
@@ -16,7 +27,38 @@ function submitPayment() {
       valid_until: document.getElementById("valid_until").value,
       card_password: document.getElementById("card_password").value,
     }),
-  }).then(function (response) {
-      
-  });
+  })
+    .then(function (response) {
+      if (response.ok) {
+        paymentProgress.hidden = true;
+        paymentSuccess.hidden = false;
+      }
+    })
+    .catch(function (error) {
+      paymentForm.hidden = false;
+      paymentErrorContainer.hidden = false;
+      paymentError.innerText = "서버와 통신 중 오류가 발생했습니다.";
+    });
+}
+
+function resetPaymentDialog(){
+  document.getElementById("card_owner").value = "";
+  document.getElementById("owner_birthday").value = "";
+  document.getElementById("owner_email").value = "";
+  document.getElementById("phone_number").value = "";
+  document.getElementById("card_number").value = "";
+  document.getElementById("valid_until").value = "";
+  document.getElementById("card_password").value = "";
+  const paymentForm = document.getElementById("payment-form");
+  const paymentProgress = document.getElementById("payment-progress");
+  const paymentSuccess = document.getElementById("payment-success");
+  const paymentError = document.getElementById("payment-error");
+  const paymentErrorContainer = document.getElementById(
+    "payment-error-container"
+  );
+  paymentForm.hidden = false;
+  paymentProgress.hidden = true;
+  paymentSuccess.hidden = true;
+  paymentError.hidden = true;
+  paymentErrorContainer.hidden = true;
 }
