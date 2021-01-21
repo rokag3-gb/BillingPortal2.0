495cd5f005feb049a9c78f18cf02c4d0537fa2be
Joseph Kim <joseph.kim@cloudmt.co.kr>
2021-01-21 17:12:13 +0900

    사용하지 않는 코드 삭제

--- portal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Organization, Membership
-
-# Register your models here.
-@admin.register(Organization, Membership)
-class OrgAdmin(admin.ModelAdmin):
-    pass
-
