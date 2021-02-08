27447eb17468adaad0fff87cbfe3281807fab22d
Joseph Kim <joseph.kim@cloudmt.co.kr>
2021-02-08 19:35:47 +0900

    url 모두 변경

--- policy/urls.py
+++ policy/urls.py
@@ -6,9 +6,12 @@ from policy import views
 urlpatterns = [
     path('confirm/', login_required(views.confirm), name='confirm'),
 
-    path('info/<int:info_number>', login_required(views.info), name='info'),
-    path('info/', views.info, name='info-latest'),
+    path('terms_of_use/<int:number>', login_required(views.terms_of_use), name='terms-of-use'),
+    path('terms_of_use/', views.terms_of_use, name='terms-of-use-latest'),
 
-    path('info-protection/<int:info_protection_number>', login_required(views.info_protection), name='info-protection'),
-    path('info-protection/', views.info_protection, name='info-protection-latest'),
+    path('info_gathering/<int:number>', login_required(views.info_gathering), name='info-gathering'),
+    path('info_gathering/', views.info_gathering, name='info-gathering-latest'),
+
+    path('using_credit_card/<int:number>', login_required(views.using_credit_card), name='using-credit-card'),
+    path('using_credit_card/', views.using_credit_card, name='using-credit-card-latest'),
 ]
