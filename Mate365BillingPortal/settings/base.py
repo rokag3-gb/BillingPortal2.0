9a37202aee07aaff4bf77156d3d6c614fa2c2061
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-07-08 17:13:36 +0900

    restapi pagination 추가

--- Mate365BillingPortal/settings/base.py
+++ Mate365BillingPortal/settings/base.py
@@ -192,6 +192,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     # ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
