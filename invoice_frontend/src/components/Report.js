ca591f517a1317c41371c1acf3e379105c1240e0
Jinhyeok Yu <jinhyeok.yu@cloudmt.co.kr>
2021-08-11 11:42:41 +0900

    Report 페이지 로딩, 에러 처리

--- invoice_frontend/src/components/Report.js
+++ invoice_frontend/src/components/Report.js
@@ -2,9 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PDFViewer } from '@react-pdf/renderer';
 import DocPDF from './DocPDF2';
+import ErrorMsg from './ErrorMsg';
+
+function ReportApp({ data }) {
+    return (
+        <PDFViewer frameBorder={0} style={{position: 'fixed', top: 0, left: 0, height: '100%', width: '100%', margin: 0}}>
+            <DocPDF data={data} />
+        </PDFViewer>
+    )
+}
+
+function ReportLoading() {
+    return (
+        <>
+            로딩중...!
+        </>
+    )
+}
 
 function Report({ match }) {
     const [data ,setData] = useState(null)
+    const [err, setErr] = useState(null)
     const id = match.params.id;
     const url = `/api/v1/invoice_report/${id}`
 
@@ -15,16 +33,19 @@ function Report({ match }) {
             })
             .catch((err) => {
                 console.log(err.response)
+                setErr(err.response.status)
             })
     },[])
-    
-    return (
-        <>
-            <PDFViewer frameBorder={0} style={{position: 'fixed', top: 0, left: 0, height: '100%', width: '100%', margin: 0}}>
-                {data && <DocPDF data={data} />}
-            </PDFViewer>
-        </>
-    )
+
+    if (err) {
+        return <ErrorMsg status={err} />
+    }
+
+    if (data) {
+        return <ReportApp data={data} />
+    } else {
+        return <ReportLoading />
+    }
 }
 
 export default Report
