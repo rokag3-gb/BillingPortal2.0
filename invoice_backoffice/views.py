0b5266cdd768aa91e2a9612cb786f39673028853
Youngbin Han <youngbin.han@cloudmt.co.kr>
2021-08-11 11:30:15 +0900

    인보이스 백엔드, 리포트 화면 로그인 된 사용자만 접근 가능하도록 수정

--- invoice_backoffice/views.py
+++ invoice_backoffice/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_sameorigin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 @xframe_options_sameorigin
 def index(request):
   return render(request, 'build/index.html')
 
+@login_required
 @xframe_options_sameorigin
 def report(request, id):
   return render(request, 'build/index.html')
