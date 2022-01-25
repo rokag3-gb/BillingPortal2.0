7324eb379d24b20c54f2819c3df1a8814d57d857
Youngbin Han <youngbin.han@cloudmt.co.kr>
2022-01-25 14:50:02 +0900

    OTEL Azure Exporter 설정 작업

--- Mate365BillingPortal/asgi.py
+++ Mate365BillingPortal/asgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.django import DjangoInstrumentor
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+
+exporter = AzureMonitorTraceExporter.from_connection_string(
+    os.environ.get("APPLICATIONINSIGHTS_CONNECTION_STRING", "")
+)
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mate365BillingPortal.settings')
 DjangoInstrumentor().instrument()
